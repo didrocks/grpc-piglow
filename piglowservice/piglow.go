c26743c168e284ad5abe0199096204dc54588074
Didier Roche <didrocks@ubuntu.com>
2016-11-30 13:01:15 +0100

    Implement Set* brightness proxy functions

--- piglowservice/piglow.go
+++ piglowservice/piglow.go
@@ -28,6 +28,41 @@ func (s *service) SetLED(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error
 	return s.apply()
 }
 
+// Set all LEDs to brightness
+func (s *service) SetAll(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetAll)
+}
+
+// SetWhite all White LEDs to brightness
+func (s *service) SetWhite(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetWhite)
+}
+
+// SetBlue all Blue LEDs to brightness
+func (s *service) SetBlue(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetBlue)
+}
+
+// SetGreen all Green LEDs to brightness
+func (s *service) SetGreen(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetGreen)
+}
+
+// SetYellow all WhYellowite LEDs to brightness
+func (s *service) SetYellow(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetYellow)
+}
+
+// SetOrange all Orange LEDs to brightness
+func (s *service) SetOrange(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetOrange)
+}
+
+// SetRed all Red LEDs to brightness
+func (s *service) SetRed(ctx context.Context, in *pb.LedRequest) (*pb.Ack, error) {
+	return s.setBrightnessWithFunc(ctx, in, s.p.SetRed)
+}
+
 // convert and ensure num led is valid
 func ensureNumLed(n int32) (int8, error) {
 	if n < 0 || n > 17 {
