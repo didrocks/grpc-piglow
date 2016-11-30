d9cf5feae0ef4ef41b1882e164fddd035fc3f2bf
Didier Roche <didrocks@ubuntu.com>
2016-11-30 16:12:57 +0100

    Move server listener and add flexible port listening
    Move the server to its own example directory.
    We start at port 3146, and then increments until we find a valid opened port.

--- examples/server/main.go
+++ examples/server/main.go
@@ -3,26 +3,34 @@ package main
 import (
 	"log"
 	"net"
+	"strconv"
 
 	"github.com/didrocks/grpc-piglow/piglowservice"
-
 	"google.golang.org/grpc"
 )
 
 //go:generate protoc -I proto/ proto/piglow.proto --go_out=plugins=grpc:proto
 func main() {
+	var l net.Listener
+	var err error
 
-	lis, err := net.Listen("tcp", ":9875")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	port := 3146
+	for l == nil {
+		l, err = net.Listen("tcp", ":"+strconv.Itoa(port))
+		if err != nil {
+			log.Printf("failed to listenn: %v\n", err)
+			port++
+		}
 	}
+	log.Printf("listening on port %d\n", port)
+
 	s := grpc.NewServer()
 
 	if err := piglowservice.RegisterPiGlowService(s); err != nil {
 		log.Fatalf("couldn't create piglow proxy: %v", err)
 	}
 
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(l); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
